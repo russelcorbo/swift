428ab8d3bef3089da1a158b54e5e6f93ef222e16
Russel Corbo <russelcorbo@gmail.com>
2023-07-18 19:42:01 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Russel Corbo on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
