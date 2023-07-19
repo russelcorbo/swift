2ba2d8c6b28297d3bd81530aa8cf93d1ef713f7b
Russel Corbo <russelcorbo@gmail.com>
2023-07-19 16:21:49 -0400

    Landmark app data

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -13,9 +13,39 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                MapView()
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                
+                CircleImage()
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack(alignment: .leading) {
+                    Text("Turtle Rock")
+                        .font(.title)
+                    
+                    HStack {
+                        Text("Joshua Tree National Park")
+                        Spacer()
+                        Text("California")
+                            
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    Divider()
+                    
+                    Text("About Turtle Rock")
+                        .font(.title2)
+                    Text("Descriptive text goes here")
+                }
+                .padding()
+                
+                Spacer()
+            }
         }
-        .padding()
+        
     }
 }
 
