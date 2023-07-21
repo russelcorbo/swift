a61c562e352ce6e1a855970112e964162363c74e
Russel Corbo <russelcorbo@gmail.com>
2023-07-21 12:43:58 -0400

    Add files via upload

---
+++ CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Russel Corbo on 7/18/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
